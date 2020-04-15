1c5631c8c75c2bfbcba57b8a07312505f0b1a140
Geferson Pillaca <gpillacag@gmail.com>
2020-04-14 21:06:41 -0500

    add custom cluster

--- app/src/main/java/com/gpillaca/mapa19/map/MarkerClusterRender.kt
+++ app/src/main/java/com/gpillaca/mapa19/map/MarkerClusterRender.kt
@@ -1,20 +1,27 @@
 package com.gpillaca.mapa19.map
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.clustering.Cluster
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.clustering.view.DefaultClusterRenderer
 import com.google.maps.android.ui.IconGenerator
+import java.util.*
 
 private const val MARKER_DIMENSION = 90
 
 class MarkerClusterRender(
-    context: Context,
-    map: GoogleMap,
+    val context: Context,
+    val map: GoogleMap,
     clusterManager: ClusterManager<PersonItem>
 ) : DefaultClusterRenderer<PersonItem>(context, map, clusterManager) {
 
@@ -34,5 +41,40 @@ class MarkerClusterRender(
         val icon = iconGenerator?.makeIcon()
         markerOptions.icon(BitmapDescriptorFactory.fromBitmap(icon))
         markerOptions.title(item.title)
+    }
+
+    override fun onBeforeClusterRendered(
+        cluster: Cluster<PersonItem>,
+        markerOptions: MarkerOptions
+    ) {
+        markerOptions.icon(getClusterIcon(cluster))
+    }
+
+    override fun onClusterUpdated(cluster: Cluster<PersonItem>, marker: Marker) {
+        marker.setIcon(getClusterIcon(cluster))
+    }
+
+    override fun shouldRenderAsCluster(cluster: Cluster<PersonItem>): Boolean {
+        return cluster.size > 1
+    }
+
+    private fun getClusterIcon(cluster: Cluster<PersonItem>): BitmapDescriptor? {
+        val profilePhotos: MutableList<Drawable> =
+            ArrayList(
+                4.coerceAtMost(cluster.size)
+            )
+
+        for (item in cluster.items) { // Draw 4 at most.
+            if (profilePhotos.size == 4) break
+            val drawable: Drawable? = ContextCompat.getDrawable(context, item.getIcon())
+            drawable?.setBounds(0, 0, MARKER_DIMENSION, MARKER_DIMENSION)
+            drawable?.let { profilePhotos.add(it) }
+        }
+
+        val multiDrawable = MultiDrawable(profilePhotos)
+        multiDrawable.setBounds(0, 0, MARKER_DIMENSION, MARKER_DIMENSION)
+        markerImageView?.setImageDrawable(multiDrawable)
+        val icon: Bitmap? = iconGenerator?.makeIcon(cluster.getSize().toString())
+        return BitmapDescriptorFactory.fromBitmap(icon)
     }
 }
